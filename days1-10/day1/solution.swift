2f097ee08906611874a2667f15e9f2c41a5626a5
Kittikorn Ariyasuk <kittikorn.a@gmail.com>
2022-04-25 19:37:46 +0700

    day 1

--- days1-10/day1/solution.swift
+++ days1-10/day1/solution.swift
@@ -1,16 +1,16 @@
 class Solution {
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
         // Dictionary key is nu,ber value is index
-        var diffDict: [Int: Int] = [:]
+        var numDict: [Int: Int] = [:]
         for (index, element) in nums.enumerated() {
-             diffDict[target - element] = index
+             numDict[element] = index
         }
         
         var firstIndex = -1
         var secondIndex = -1
         
         for (index, element) in nums.enumerated() {
-            if let pairIndex = diffDict[element],  pairIndex != index {
+            if let pairIndex = numDict[target - element],  pairIndex != index {
                 firstIndex = index
                 secondIndex = pairIndex
                 break
